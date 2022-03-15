562228e21845cec3e50360878c4ed29a47048018
Maria Neumayer <maria.neumayer@skyscanner.net>
2022-03-15 10:10:01 +0000

    Add pre-release flow

--- bump_version.py
+++ bump_version.py
@@ -4,7 +4,7 @@ import sys
 file_path = "version.json"
 new_version = sys.argv[1]
 
-version_regex = re.compile(r"(\"version\":\s*['\"])(\d+\.\d+\.\d+)")
+version_regex = re.compile(r"(\"version\":\s*['\"])((([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)")
 with open(file_path, "r+") as f:
     content = f.read()
     f.seek(0)
