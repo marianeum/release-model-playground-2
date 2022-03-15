b8f4e4028fc6a32f2f198d9832325fcac8f659c7
Maria Neumayer <maria.neumayer@skyscanner.net>
2022-03-15 09:47:24 +0000

    Fix json format

--- bump_version.py
+++ bump_version.py
@@ -4,7 +4,7 @@ import sys
 file_path = "version.json"
 new_version = sys.argv[1]
 
-version_regex = re.compile(r"(version\s*=\s*['\"])(\d+\.\d+\.\d+)")
+version_regex = re.compile(r"(\"version\":\s*['\"])(\d+\.\d+\.\d+)")
 with open(file_path, "r+") as f:
     content = f.read()
     f.seek(0)
